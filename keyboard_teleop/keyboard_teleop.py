0cf8efcb0766f0be082422d49b2fa5cacd9649f7
Kenta Hirachi <s23c1114qb@s.chibakoudai.jp>
2025-01-05 23:47:18 +0900

    test

--- keyboard_teleop/keyboard_teleop.py
+++ keyboard_teleop/keyboard_teleop.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-# SPDX-FileCopyrightText: 2024 Kenta Hirachi
+# SPDX-FileCopyrightText: 2025 Kenta Hirachi
 # SPDX-License=Identifier: Apache 2.0
 
 import rclpy
--- test/test.bash
+++ test/test.bash
@@ -1,17 +1,18 @@
 #!/bin/bash
-# SPDX-FileCopyrightText: 2024 Kenta Hirachi
+# SPDX-FileCopyrightText: 2025 Kenta Hirachi
 # SPDX-License-Identifier: Apache-2.0
 
 dir=~
 [ "$1" != "" ] && dir="$1"
 
 cd $dir/ros2_ws
-colcon build
+colcon build|| { echo "Build failed"; exit 1; }
 source $dir/.bashrc
 
-timeout 60 ros2 run keyboard_teleop keyboard_teleop | tee /tmp/keyboard_teleop.log
+SESSION_NAME="teleop_session"
+tmux new-session -d -s $SESSION_NAME "ros2 run keyboard_teleop keyboard_teleop > $HOME/tmp/keyboard_teleop.log 2>&1"
+sleep 5
 ros2 topic pub /cmd_vel geometry_msgs/msg/Twist "{linear: {x: 0.0, y: -0.5, z: 0.0}, angular: {x: 0.0, y: 0.0, z: 0.0}}" --once
-echo "Waiting for messages on 'cmd_vel' topic..."
 ros2 topic echo /cmd_vel -n 1 | tee /tmp/cmd_vel_output.log
 
 cat /tmp/cmd_vel_output.log
